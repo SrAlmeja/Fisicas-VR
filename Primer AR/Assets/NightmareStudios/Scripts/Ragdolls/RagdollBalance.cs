a6ec846e7b68255ce7b52878c5f901eafd9cb4a2
Sr Almeja <cdmx1895@amerike.edu.mx>
2022-10-05 16:55:00 -0500

    Se agrego un ejemplo de como resolver el active Raggdoll

---
+++ Primer AR/Assets/NightmareStudios/Scripts/Ragdolls/RagdollBalance.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace  RagdollBalance
+{
+    public class RagdollBalance : MonoBehaviour
+    {
+        [SerializeField] private int solverInteractions = 12;
+        [SerializeField]
+    }    
+}
+
